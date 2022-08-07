2cff89feeca24ca84991e1d490a8f8b2ce77359f
Michael Smith <smith.michael.oliver@gmail.com>
2022-08-07 20:45:47 +1000

    Updated Clients Index to be a table
    Updated the index view to render a table using DIVs and styled with Tailwindcss
    Hooked up the Hotwire Turbo Streams to continue to work

--- app/models/client.rb
+++ app/models/client.rb
@@ -23,5 +23,6 @@ class Client < ApplicationRecord
   # Broadcast changes in realtime with Hotwire
   after_create_commit  -> { broadcast_prepend_later_to :clients, partial: "clients/index", locals: { client: self } }
   after_update_commit  -> { broadcast_replace_later_to self }
+  # after_update_commit  -> { broadcast_replace_later_to :client, partial: client, locals: { client: self } }
   after_destroy_commit -> { broadcast_remove_to :clients, target: dom_id(self, :index) }
 end
