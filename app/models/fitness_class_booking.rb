289f8218b9ceb8d442d89381621cad614dae77a0
Michael Smith <smith.michael.oliver@gmail.com>
2022-08-09 20:18:18 +1000

    Add validations for fitness class bookings
    make sure that a client can only be added to a fitness_class_schedule once

--- app/models/fitness_class_booking.rb
+++ app/models/fitness_class_booking.rb
@@ -2,6 +2,9 @@ class FitnessClassBooking < ApplicationRecord
   belongs_to :client
   belongs_to :fitness_class_schedule
 
+  validates :client_id, uniqueness: { scope: :fitness_class_schedule_id, message: ": A booking already exists for this client in this class." }
+  validates :fitness_class_schedule_id, uniqueness: { scope: :client_id, message: ": A booking already exists for this client in this class." }
+
   after_create_commit -> {
     broadcast_update_later_to self.fitness_class_schedule,
     target: "#{dom_id(self.fitness_class_schedule)}_bookings_count",
