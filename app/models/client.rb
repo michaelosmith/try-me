a703425f98d2d3f838eb1511766eda6ddc633ae1
Michael Smith <smith.michael.oliver@gmail.com>
2022-08-28 13:46:53 +1000

    Update Client model
    - add instance method to find if client has had a contract
    - add class method to find clients on contract

--- app/models/client.rb
+++ app/models/client.rb
@@ -30,13 +30,23 @@ class Client < ApplicationRecord
 
   scope :lifetime_value, ->(client) { Sale.service_sale.where(client_id: client.id).select(:total_amount).sum(:total_amount).to_f }
 
-
+  # Class methods
   def self.avg_lifetime_value
     Sale.service_sale.select(:total_amount).sum(:total_amount).to_f / self.count
   end
+
+  def self.contract_clients
+    ClientContract.select(:client_id).where(client_id: self.ids).distinct
+  end
+
+  # Instance methods
+  def has_had_contract?
+    ClientContract.where(client_id: self.id).any?
+  end
+
+
   # Broadcast changes in realtime with Hotwire
-  after_create_commit  -> { broadcast_prepend_later_to :clients, partial: "clients/index", locals: { client: self } }
-  after_update_commit  -> { broadcast_replace_later_to self }
-  # after_update_commit  -> { broadcast_replace_later_to :client, partial: client, locals: { client: self } }
-  after_destroy_commit -> { broadcast_remove_to :clients, target: dom_id(self, :index) }
+  # after_create_commit  -> { broadcast_prepend_later_to :clients, partial: "clients/index", locals: { client: self } }
+  # after_update_commit  -> { broadcast_replace_later_to self }
+  # after_destroy_commit -> { broadcast_remove_to :clients, target: dom_id(self, :index) }
 end
